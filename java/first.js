c012ab137a33feea5266a32be8566784b6983cb2
Bilaj583 <bilajshahzad@gmail.com>
2024-12-09 14:42:16 +0500

    update

---
+++ java/first.js
@@ -0,0 +1,38 @@
+
+// // var food ="hot";
+// // if(food=="hot"){
+// //     console.log('Too Hot')
+// // }
+// // else if(food=="cold"){
+// //     console.log('Too cold')
+// // }
+// // else{
+// //     console.log('just right')
+// // }
+// var place='first';
+// switch(place){
+//     case 'first':
+//         console.log("Gold")
+//         break;
+//     case 'second':
+//         console.log("Silver")
+//         break;
+//     case 'third':
+//         console.log("Brown")
+//         break;
+//      default:
+//         console.log("No medal")
+// }
+var age =67;
+if(age>=65){
+    console.log("you get your income from your pension")
+}
+else if(age<=65 && age >=18 ){
+    console.log("Each month you get your salary")
+}
+else if(age <18){
+    console.log("You get an allownce")
+}
+else{
+    console.log("The value of the age variable is not numerical")
+}
